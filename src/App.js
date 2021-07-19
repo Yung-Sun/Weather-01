72ceab1f5e1c879b7954c54cb640dd913c227bf4
yungsun <yungsun3307@163.com>
2021-07-19 14:43:39 +0800

    😎 add react-router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import './App.scss';
-import styled from 'styled-components'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-
-  h1 {
-    color: red;
-    font-size: 1.5em;
-    text-align: center;
-  }
-`;
 
 function App() {
+    function Home() {
+        return <h2>Home</h2>;
+    }
+
+    function About() {
+        return <h2>About</h2>;
+    }
+
+    function Users() {
+        return <h2>Users</h2>;
+    }
 
     return (
-        <Wrapper>
-            <h1>
-                Hello World!
-            </h1>
-        </Wrapper>
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/users">Users</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/users">
+                        <Users/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
