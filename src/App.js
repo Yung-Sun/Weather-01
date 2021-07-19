0dd045acd75abdb75f124c13c3d6f35c88d7a3a1
容新 <yungsun3307@163.com>
2021-07-19 17:48:25 +0800

    😎 add antd

--- src/App.js
+++ src/App.js
@@ -1,58 +1,67 @@
 import './App.scss';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
 } from "react-router-dom";
+import {Button} from 'antd';
+import styled from 'styled-components'
 
 
 function App() {
-    function Home() {
-        return <h2>Home</h2>;
-    }
-
-    function About() {
-        return <h2>About</h2>;
-    }
-
-    function Users() {
-        return <h2>Users</h2>;
-    }
-
+  function Home() {
     return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">About</Link>
-                        </li>
-                        <li>
-                            <Link to="/users">Users</Link>
-                        </li>
-                    </ul>
-                </nav>
+      <Wrapper>
+        <h2>Home</h2>
+      </Wrapper>
+    )
+  }
 
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                <Switch>
-                    <Route path="/about">
-                        <About/>
-                    </Route>
-                    <Route path="/users">
-                        <Users/>
-                    </Route>
-                    <Route path="/">
-                        <Home/>
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+  function About() {
+    return <h2>About</h2>;
+  }
+
+  function Users() {
+    return <h2>Users</h2>;
+  }
+
+  const NavWrapper = styled.section`
+    display: flex;
+    align-items: center;
+    width: 30%;
+  `
+  const Wrapper = styled.div`
+    background: #666;
+  `
+
+  return (
+    <Router>
+      <NavWrapper>
+        <Button>
+          <Link to="/">Home</Link>
+        </Button>
+        <Button>
+          <Link to="/about">About</Link>
+        </Button>
+        <Button>
+          <Link to="/users">Users</Link>
+        </Button>
+      </NavWrapper>
+      {/* Switch是渲染区域*/}
+      <Switch>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/users">
+          <Users/>
+        </Route>
+        <Route path="/">
+          <Home/>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
